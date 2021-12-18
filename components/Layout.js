426ca2877dcc997812ede14059703d56e7b4e454
yhunlu <yhunlu87@gmail.com>
2021-12-18 21:28:06 +0300

    added layout with styles and fixed material-ui bug

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,23 @@
 import { AppBar, Container, Toolbar, Typography } from '@mui/material';
 import Head from 'next/head';
 import React from 'react';
+import useStyles from './../utils/styles';
 
 export default function Layout({ children }) {
+  const classes = useStyles();
+
   return (
     <div>
       <Head>
         <title>Next CarsiBazaar</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>CarsiBazaar</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
         <Typography>All rights reserved. Next CarsiBazaar</Typography>
       </footer>
     </div>
