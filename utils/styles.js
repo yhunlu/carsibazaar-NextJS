7e60e1844623fc41b8705c1bc8b50627b9774644
yhunlu <yhunlu87@gmail.com>
2021-12-19 00:25:11 +0300

    list of products with sample data

--- utils/styles.js
+++ utils/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
   navbar: {
-    backgroundColor: '#203040',
+    backgroundColor: '#203040e8',
     '& a': {
       color: '#ffffff',
       marginLeft: 10,
